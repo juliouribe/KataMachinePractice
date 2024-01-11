625fb683631bdebdf6ab775ab23172a8c7d6a162
Julio Uribe <uribejulioc@gmail.com>
2024-01-11 13:36:42 -0800

    Finish binary search

---
+++ src/day1/BinarySearchList.ts
@@ -0,0 +1,27 @@
+// Binary search is great when the dataset is ordered
+
+export default function bs_list(haystack: number[], needle: number): boolean {
+  // Binary search using a while loop
+  let low = 0;
+  let high = haystack.length;
+  let mid;
+
+  do {
+    /*
+      Mid point is defined as the low (relative starting index) plus the offset.
+      Low can change through the search. The (high-low)/2 is the offset relative
+      to where the current start and ends are.
+    */
+    mid = Math.floor(low + (high-low)/2);
+    let value = haystack[mid];
+    if (value === needle) {
+      return true;
+    } else if (value < needle) {
+      high = mid;
+    } else {
+      low = mid + 1;
+    }
+  } while (low < high)
+
+  return false;
+}
