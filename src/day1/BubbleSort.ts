36503459878dce09de186dc50bbf4d4772a34ce3
Julio Uribe <uribejulioc@gmail.com>
2024-01-18 13:54:42 -0800

    Add code for a Queue class

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -4,13 +4,13 @@ export default function bubble_sort(arr: number[]): void {
     Look at the start index, are you larger than the right? If yes, swap. i++
     Keep going until we're only looking at one element.
   */
- for (let i = 0; i < arr.length; i++) {
-  for (let j = 0; j < arr.length - 1 - i ; j++) {
-    if (arr[j] > arr[j + 1]) {
-      const temp = arr[j];
-      arr[j] = arr[j + 1];
-      arr[j + 1] = temp;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
     }
   }
- }
 }
