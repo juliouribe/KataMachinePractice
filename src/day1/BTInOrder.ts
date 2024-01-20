841d2ce305c1a4999f204bae499e64d275b4678b
Julio Uribe <uribejulioc@gmail.com>
2024-01-19 18:56:20 -0800

    Add tree taversals

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,15 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function traverse(node: BinaryNode<number> | null, path: number[]): number[] {
+  if (!node) {
+    return path;
+  }
 
+  traverse(node.left, path);
+  path.push(node.value);
+  traverse(node.right, path);
+
+  return path;
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+  return traverse(head, []);
 }
