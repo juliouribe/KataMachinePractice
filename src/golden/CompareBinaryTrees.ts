8eba0d1341af945ceee4444a3c0799e9e951a38f
Julio Uribe <uribejulioc@gmail.com>
2024-01-21 09:21:19 -0800

    Add compare trees

--- src/golden/CompareBinaryTrees.ts
+++ src/golden/CompareBinaryTrees.ts
@@ -1,16 +1,17 @@
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
-  // Base cases
-  // If both are null, then we are good.
+  // If both are empty return
   if (a === null && b === null) {
     return true;
   }
-  // If either is empty then return false. Previous base cases establishes some value in the other.
+
+  // A exists or B don't exist.
   if (a === null || b === null) {
     return false;
   }
-  // If they are not equal return false.
+
   if (a.value !== b.value) {
     return false;
   }
+
   return compare(a.left, b.left) && compare(a.right, b.right);
 }
