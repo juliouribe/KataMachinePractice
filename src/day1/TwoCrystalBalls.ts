8c9c4ebde013a7686a5896fe8a6188e3eba8efe0
Julio Uribe <uribejulioc@gmail.com>
2025-09-10 10:29:38 -0700

    Two Cystal Balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,25 +1,22 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-  /*
-    Take the square root of N and do one step at a time. Once you find a break
-    then start iterating one index at a time.
-  */
-  const jumpAmount = Math.sqrt(Math.floor(breaks.length));
-  let lastSpot = jumpAmount;
+    const jump = Math.floor(Math.sqrt(breaks.length));
+    let l = 0;
+    let r = jump - 1;
 
-  // Find the first break using steps of sqrt(N)
-  while (lastSpot < breaks.length) {
-    // Once we find a break, go back to the previous safe spot and exit.
-    if (breaks[lastSpot]) {
-      lastSpot -= jumpAmount;
-      break;
-    }
-    lastSpot += jumpAmount;
-  }
-  // Increment one at a time from last safe spot up to
-  for (let i = lastSpot + 1; i < (lastSpot + jumpAmount) && i < breaks.length; i++) {
-    if (breaks[i]) {
-      return i;
-    }
-  }
-  return -1
+    /*
+    l = 0, 7
+    r = 6, 13
+    t = 12
+    */
+    do {
+        if (breaks[r]) {
+            for (let i = l; i <= r; i++) {
+                if (breaks[i]) return i;
+            }
+        }
+        l += jump;
+        r += jump;
+    } while (l < breaks.length);
+
+    return -1;
 }
