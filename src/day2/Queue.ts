ef2d597b82a4176a2cd3d6edfb46484ec475ce74
Julio Uribe <uribejulioc@gmail.com>
2025-09-11 08:22:02 -0700

    Quick review

--- src/day2/Queue.ts
+++ src/day2/Queue.ts
@@ -1,18 +1,46 @@
+type Node<T> = {
+    val: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        this.length++;
+        const node = { val: item } as Node<T>;
+        if (!this.tail) {
+            this.head = this.tail = node;
+            return;
+        }
+        this.tail.next = node;
+        this.tail = node;
+    }
 
-}
     deque(): T | undefined {
+        if (!this.head) {
+            return;
+        }
 
-}
+        this.length--;
+        const node = this.head;
+        if (this.length === 0) {
+            this.head = this.tail = undefined;
+        } else {
+            this.head = node.next;
+        }
+
+        return node.val;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.val;
+    }
 }
--- src/day2/Stack.ts
+++ src/day2/Stack.ts
@@ -1,18 +1,40 @@
+type Node<T> = {
+    val: T;
+    next?: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
+        this.length++;
+        const node = { val: item } as Node<T>;
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+        node.next = this.head;
+        this.head = node;
+    }
 
-}
     pop(): T | undefined {
+        if (!this.head) {
+            return;
+        }
 
-}
+        this.length--;
+        const node = this.head;
+        this.head = node.next;
+        return node.val;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.val;
+    }
 }
