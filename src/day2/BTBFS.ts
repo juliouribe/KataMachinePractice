d1531154c229f4d051609e66a6dddfdea53756d7
Julio Uribe <uribejulioc@gmail.com>
2025-09-18 11:52:47 -0700

    BTBFS

--- src/day2/BTBFS.ts
+++ src/day2/BTBFS.ts
@@ -1,3 +1,13 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q: BinaryNode<number>[] = [head];
 
+    while (q.length) {
+        const curr = q.shift() as BinaryNode<number>;
+        if (curr.value === needle) return true;
+
+        if (curr.left) q.push(curr.left);
+        if (curr.right) q.push(curr.right);
+    }
+
+    return false;
 }
